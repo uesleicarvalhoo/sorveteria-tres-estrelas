4ef94d6f845da8be873311d220e3109d7aa813fe
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2022-12-09 23:16:45 -0300

    chore: adjust products domain

--- internal/http/fiber/handler/products.go
+++ internal/http/fiber/handler/products.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/uesleicarvalhoo/sorveteria-tres-estrelas/internal/http/dto"
@@ -12,6 +14,7 @@ func MakeProductsRoutes(r fiber.Router, svc products.UseCase) {
 	r.Get("/", getAllProducts(svc))
 	r.Post("/", createProduct(svc))
 	r.Delete("/:id", deleteProductByID(svc))
+	r.Post("/:id", updateProductByID(svc))
 }
 
 // @Summary     Get Product by ID
@@ -99,7 +102,7 @@ func deleteProductByID(svc products.UseCase) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		id, err := uuid.Parse(c.Params("id"))
 		if err != nil {
-			return c.Status(fiber.StatusUnprocessableEntity).JSON(dto.MessageJSON{Message: err.Error()})
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(dto.MessageJSON{Message: "invalid product id"})
 		}
 
 		err = svc.Delete(c.Context(), id)
@@ -108,5 +111,37 @@ func deleteProductByID(svc products.UseCase) fiber.Handler {
 		}
 
 		return c.SendStatus(fiber.StatusAccepted)
+	}
+}
+
+// Summary     Update product by ID
+// Description Update product
+// Tags        Product
+// Accept      json
+// Produce     json
+// Param       id path string true "the id of product"
+// Param       payload body products.UpdatePayload true "the product data"
+// Success     200 {object} products.Product
+// Failure     422 {object} dto.MessageJSON "when data is invalid"
+// Failure     500 {object} dto.MessageJSON "when an error occurs"
+// Router      /products/{id} [put]
+func updateProductByID(svc products.UseCase) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id, err := uuid.Parse(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(dto.MessageJSON{Message: "invalid product id"})
+		}
+
+		var payload products.UpdatePayload
+		if err := c.BodyParser(&payload); err != nil {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(dto.MessageJSON{Message: err.Error()})
+		}
+
+		p, err := svc.Update(c.Context(), id, payload)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(dto.MessageJSON{Message: err.Error()})
+		}
+
+		return c.Status(http.StatusOK).JSON(p)
 	}
 }
---
+++ products/dto.go
@@ -0,0 +1,17 @@
+package products
+
+import "errors"
+
+var ErrNoDataForUpdate = errors.New("no data for update")
+
+type UpdatePayload struct {
+	Value         float32 `json:"value"`
+	Name          string  `json:"name"`
+	PriceVarejo   float64 `json:"price_varejo"`
+	PriceAtacado  float64 `json:"price_atacado"`
+	AtacadoAmount int     `json:"atacado_amount"`
+}
+
+func (up UpdatePayload) IsEmpty() bool {
+	return up.Value == 0 && up.Name == "" && up.PriceVarejo == 0 && up.PriceAtacado == 0 && up.AtacadoAmount == 0
+}
