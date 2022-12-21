b03eb28c0f28a8b9259749c90f551233151ef9ec
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2022-12-21 18:17:23 -0300

    feat: implement version to healthcheck service

--- healthcheck/service.go
+++ healthcheck/service.go
@@ -1,16 +1,22 @@
 package healthcheck
 
-import "context"
+import (
+	"context"
+
+	"github.com/uesleicarvalhoo/sorveteria-tres-estrelas/config"
+)
 
 type Service struct {
+	cfg   *config.Config
 	db    DatabasePing
 	cache CachePing
 }
 
 var _ UseCase = Service{}
 
-func NewService(db DatabasePing, cache CachePing) Service {
+func NewService(cfg *config.Config, db DatabasePing, cache CachePing) Service {
 	return Service{
+		cfg:   cfg,
 		db:    db,
 		cache: cache,
 	}
@@ -18,19 +24,20 @@ func NewService(db DatabasePing, cache CachePing) Service {
 
 func (s Service) HealthCheck(ctx context.Context) HealthStatus {
 	status := HealthStatus{
-		App: StatusUp,
+		Version: s.cfg.ServiceVersion,
+		Status:  StatusUp,
 	}
 
 	if err := s.db.Ping(); err != nil {
 		status.Database = StatusDown
-		status.App = StatusDown
+		status.Status = StatusDown
 	} else {
 		status.Database = StatusUp
 	}
 
 	if err := s.cache.Ping(ctx); err != nil {
 		status.Cache = StatusDown
-		status.App = StatusDown
+		status.Status = StatusDown
 	} else {
 		status.Cache = StatusUp
 	}
