160d85631e6cf2e83059e30ef54e26250981d385
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2023-03-28 22:11:40 -0300

    chore: remove backend healthcheck (#64)
    * chore: remove backend healthcheck
    
    * fix: frontend environments

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,9 +1,5 @@
 export const itemsPerPage = process.env.ITEMS_PER_PAGE || 10
-
-const envApiUrl = process.env.VUE_APP_API_URL || `${window.location.protocol}//${window.location.host}/api`
-
-export const apiUrl = envApiUrl
-
-export const serviceName = process.env.SERVICE_NAME || "frontend"
-export const serviceVersion = process.env.SERVICE_VERSION || "0.0.0"
-export const otelEndpoint = process.env.OTEL_URL || "http://localhost:9411/api/v2/spans"
+export const apiUrl = process.env.VUE_APP_API_URL || `${window.location.protocol}//${window.location.host}/api`
+export const serviceName = process.env.VUE_APP_SERVICE_NAME || "frontend"
+export const serviceVersion = process.env.VUE_APP_SERVICE_VERSION || "0.0.0"
+export const otelEndpoint = process.env.VUE_APP_OTEL_URL || "http://localhost:9411/api/v2/spans"
