5b9d14a54da6c98d30f3e040c7eb41aee1f3b28a
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2023-03-28 22:26:52 -0300

    fix: add frontend environment variables to Dockerfile (#65)

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,4 @@
-export const itemsPerPage = process.env.ITEMS_PER_PAGE || 10
+export const itemsPerPage = process.env.VUE_APP_ITEMS_PER_PAGE || 10
 export const apiUrl = process.env.VUE_APP_API_URL || `${window.location.protocol}//${window.location.host}/api`
 export const serviceName = process.env.VUE_APP_SERVICE_NAME || "frontend"
 export const serviceVersion = process.env.VUE_APP_SERVICE_VERSION || "0.0.0"
